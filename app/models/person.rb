553717622f1f28217f0010f478f2bfc3ad669a49
Maytee Chinavanichkit <maytee90@gmail.com>
2012-02-19 17:25:05 -0500

    form people with pulldown

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,10 +2,10 @@ class Person < ActiveRecord::Base
   #need validate here
   validates :first_name, :last_name, :presence => true;
   
-  GENDER_TYPE = [ "Male", "Female" ]
-  ETHNICITY_TYPE = [ "Hispanic", "African-American", "Caucasian", "Native American", "Other" ]
-  INSURANCE_TYPE = [ "Yes - Private", "Yes - Medicaid (Access)", "Yes - Medicare", "No" ]
-  HOUSING_TYPE = [ "Rent", "Rent-subsidized (housing/Section 8)", "Home owner", "Homeless", "Doubled-up (staying somewhere temporarily)" ]
-  FAMILY_TYPE = ["Single (no children)", "2 adults (no children)", "Single parent (w/ children)", "2 parents (w/ children)"]
+  GENDER_TYPES = [ "Male", "Female" ]
+  ETHNICITY_TYPES = [ "Hispanic", "African-American", "Caucasian", "Native American", "Other" ]
+  INSURANCE_TYPES = [ "Yes - Private", "Yes - Medicaid (Access)", "Yes - Medicare", "No" ]
+  HOUSING_TYPES = [ "Rent", "Rent-subsidized (housing/Section 8)", "Home owner", "Homeless", "Doubled-up (staying somewhere temporarily)" ]
+  FAMILY_TYPES = ["Single (no children)", "2 adults (no children)", "Single parent (w/ children)", "2 parents (w/ children)"]
   
 end
