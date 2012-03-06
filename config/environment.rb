90dbb5400fab98bce61fd37f75e37821feaecda0
Maytee Chinavanichkit <maytee90@gmail.com>
2012-03-06 00:26:17 -0500

    change default time format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 NBM::Application.initialize!
+
+# Change the default date format
+Date::DATE_FORMATS[:default] = "%B %d, %Y"
+Time::DATE_FORMATS[:default] = "%B %d, %Y %H:%M"
