9ed9646bce1e1fdfbc5a4145cfd80f2217532472
Maytee Chinavanichkit <maytee90@gmail.com>
2012-03-06 23:00:39 -0500

    fixed with eastern standard time

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -9,8 +9,8 @@ class ReportController < ApplicationController
     if params[:start].nil?
       @food_visit = nil
     else
-      atStart = DateTime.civil(params[:start]["(1i)"].to_i,params[:start]["(2i)"].to_i,params[:start]["(3i)"].to_i, Rational(-6, 24))
-      atEnd = DateTime.civil(params[:end]["(1i)"].to_i,params[:end]["(2i)"].to_i,params[:end]["(3i)"].to_i, Rational(-6, 24))
+      atStart = DateTime.civil(params[:start]["(1i)"].to_i,params[:start]["(2i)"].to_i,params[:start]["(3i)"].to_i, Rational(-5, 24))
+      atEnd = DateTime.civil(params[:end]["(1i)"].to_i,params[:end]["(2i)"].to_i,params[:end]["(3i)"].to_i, Rational(-5, 24))
 
       @food_visits = FoodVisit.all( :select => "created_at, SUM(number_of_adults) number_of_adults, SUM(number_of_children) number_of_children, SUM(number_of_elderly) number_of_elderly", :conditions => ["created_at between ? and ? and status = ?", atStart.to_time, atEnd.to_time, FoodVisit::STATUS_TYPES[1]], :group => ["DATE(created_at)"])
     end
