6c48c6bfa085f44a197fe91a628d926ffdb8a2e8
Maytee Chinavanichkit <maytee90@gmail.com>
2012-03-08 11:48:45 -0500

    bad date bug fixed

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -8,17 +8,22 @@ class ReportController < ApplicationController
   
   def food_visit
     if params[:start_date].nil? or params[:start_date].empty? or params[:end_date].empty?
-      @query_humanized = "Unable to process query"
+      @query_humanized = "Unable to search. Please check the dates"
       @food_visit = nil
     else
-      atStart = Time.strptime(params[:start_date], '%m/%d/%Y').getutc
-      atEnd = Time.strptime(params[:end_date], '%m/%d/%Y').getutc
-      if atEnd <= atStart
-        @query_humanized = "Unable to search. End date is before beginning date"
-      else
-        @food_visits = FoodVisit.all( :select => "created_at, SUM(number_of_adults) number_of_adults, SUM(number_of_children) number_of_children, SUM(number_of_elderly) number_of_elderly", :conditions => ["created_at between ? and ? and status = ?", atStart.to_time, atEnd.to_time, FoodVisit::STATUS_TYPES[1]], :group => ["DATE(created_at)"])
-        @query_humanized = "Generating report of #{distance_of_time_in_words(atStart,atEnd)} from #{atStart.to_date} to #{atEnd.to_date}"
+      begin
+        atStart = Time.strptime(params[:start_date], '%m/%d/%Y').getutc
+        atEnd = Time.strptime(params[:end_date], '%m/%d/%Y').getutc
+        if atEnd <= atStart
+          @query_humanized = "Unable to search. End date must be at least one day after the start date"
+        else
+          @food_visits = FoodVisit.all( :select => "created_at, SUM(number_of_adults) number_of_adults, SUM(number_of_children) number_of_children, SUM(number_of_elderly) number_of_elderly", :conditions => ["created_at between ? and ? and status = ?", atStart.to_time, atEnd.to_time, FoodVisit::STATUS_TYPES[1]], :group => ["DATE(created_at)"])
+          @query_humanized = "Generating report of #{distance_of_time_in_words(atStart,atEnd)} from #{atStart.to_date} to #{atEnd.to_date}"
+        end
+      rescue
+        @query_humanized = "Unable to search. Please check the dates"
       end
+
     end
     
     respond_to do |format|
