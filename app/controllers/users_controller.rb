415603861f05d0e21af52e4ba9bd3006e9e29647
Maytee Chinavanichkit <maytee90@gmail.com>
2012-04-11 16:17:09 -0400

    add user does not sign user in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,8 +50,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        sign_in @user
-        flash[:success] = "Welcome"
+
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
